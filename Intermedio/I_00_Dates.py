f9600949f934e35a38584f1f0dacb6f14dfe0125
manuel101284 <manuel101284@gmail.com>
2023-04-27 23:33:28 -0500

    V.0.1.5 Crear listas por comprensión

--- Intermedio/I_00_Dates.py
+++ Intermedio/I_00_Dates.py
@@ -19,3 +19,62 @@ print("segundo", fecha_actual.second)
 timestamp = fecha_actual.timestamp()
 print(timestamp)
 
+year_2023 = datetime(2023, 1, 1)
+
+def mostrar_fecha(date):
+    print("año", date.year)
+    print("mes", date.month)
+    print("día", date.day)
+    print("hora", date.hour)
+    print("minuto", date.minute)
+    print(date.timestamp())
+
+mostrar_fecha(year_2023)
+
+# tIme ofrece otras funciones del modulo datetime
+# Es un objeto vacío, que no se asocia a nada, sirve para encapsular tiempos
+# Debe ser llenado con algún dato
+from datetime import time
+
+tiempo_actual = time()
+print(tiempo_actual)
+print(tiempo_actual.hour)
+print(tiempo_actual.minute)
+print(tiempo_actual.second)
+
+# Inicializado con datos
+tiempo_actual = time(15, 25, 8)
+print(tiempo_actual)
+print(tiempo_actual.hour)
+print(tiempo_actual.minute)
+print(tiempo_actual.second)
+
+# date ofrece otras funciones del modulo datetime
+# Debe ser inicializado siempre, con datos que deseemos o con algun dato automático
+from datetime import date
+
+# Al iniciar con la fecha del día de hoy
+fecha_ahora = date.today()
+print(fecha_ahora)
+print(fecha_ahora.year)
+print(fecha_ahora.month)
+print(fecha_ahora.day)
+
+# Al iniciar con datos que queremos 
+fecha_ahora = date(2020, 5, 14)
+print(fecha_ahora)
+print(fecha_ahora.year)
+print(fecha_ahora.month)
+print(fecha_ahora.day)
+
+# Operaciones con Fechas (SOLO SI ESTÁN DEFINIDAS DE LA MISMA FORMA)
+diff = year_2023 - fecha_actual
+print(diff)
+
+# El timedelta se usa sobretodo para trabajar con FRANJAS DE FECHAS
+from datetime import timedelta
+
+start_timedelta = timedelta(200, 100, 100, weeks = 10 )
+end_timedelta = timedelta(300, 100, 100, weeks = 13)
+print(end_timedelta - start_timedelta)
+print(end_timedelta + start_timedelta)
