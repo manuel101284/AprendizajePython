c8faa90b2be9f588ac0c91e5afc5c428ee014101
manuel101284 <manuel101284@gmail.com>
2023-05-18 15:36:31 -0500

    V.0.2.9 Login en Deta Space e instalación del Cliente para el despligue en remoto

--- Backend/FastAPI/routers/users_db.py
+++ Backend/FastAPI/routers/users_db.py
@@ -15,7 +15,7 @@ router = APIRouter(prefix="/usersdb",
 
 @router.get("/", response_model=list[User])
 async def users():
-    return users_schema(db_client.local.users.find())
+    return users_schema(db_client.users.find())
 
 
 # Path - Ruta
@@ -40,9 +40,9 @@ async def user(user: User):
     user_dict = dict(user)
     del user_dict["id"]
 
-    id = db_client.local.users.insert_one(user_dict).inserted_id
+    id = db_client.users.insert_one(user_dict).inserted_id
 
-    new_user = user_schema(db_client.local.users.find_one({"_id":id}))
+    new_user = user_schema(db_client.users.find_one({"_id":id}))
     
     return User(**new_user)
 
@@ -54,7 +54,7 @@ async def user(user: User):
     del user_dict["id"]
 
     try:
-        db_client.local.users.find_one_and_replace({"_id": ObjectId(user.id)}, user_dict)
+        db_client.users.find_one_and_replace({"_id": ObjectId(user.id)}, user_dict)
     except:
         return {"Error": "No se ha actualizado el usuario"}
     
@@ -65,7 +65,7 @@ async def user(user: User):
 @router.delete("/{id}", status_code = status.HTTP_204_NO_CONTENT)
 async def user(id: str):
 
-    found = db_client.local.users.find_one_and_delete({"_id": ObjectId(id)})
+    found = db_client.users.find_one_and_delete({"_id": ObjectId(id)})
 
     if not found:
         return {"Error": "No se ha eliminado ningún usuario"}
@@ -74,7 +74,7 @@ async def user(id: str):
 
 def search_user(field: str, key):
     try: 
-        user =  db_client.local.users.find_one({field: key})
+        user =  db_client.users.find_one({field: key})
         return User(**user_schema(user))
     except:
         return {"Error": "No existe el usuario"}
